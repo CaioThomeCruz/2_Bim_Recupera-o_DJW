b6dbb6d4662cb90976543957a8bc9d0a3637e24c
Caio Thomé Cruz <caio.cruz41@etec.sp.gov.br>
2021-06-25 12:27:49 -0300

    Inclusão dos Arquivos
    Recuperação DJW

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+const canvas = document.getElementById('field');
+
+const ctx = canvas.getContext('2d');
+
+
+
+
+let field = new Starfield(ctx);
+field.inicializa();
+field.start();
+
+
+
+
+
+
---
+++ js/starfield.js
@@ -0,0 +1,138 @@
+
+class Starfield
+{
+	
+	
+	fps ;
+    canvas ;
+    width ;
+	height ;
+    minVelocity ;
+    maxVelocity ;
+    qtdestars ;
+	stars;
+    intervalId ;
+	ctx;
+	star;
+	
+	
+	 
+  
+  
+	
+	  constructor(ctx) {
+		  this.fps = 20;
+	
+	this.width = 0;
+	this.height = 0;
+	this.minVelocity = 10;
+	this.maxVelocity = 35;
+	this.qtdestars = 200;
+	this.intervalId = 0;
+
+    this.ctx = ctx;
+	
+	this.width = window.innerWidth;
+	console.table(window.innerWidth);
+	this.height = window.innerHeight;
+	console.table(window.innerHeight);
+	this.ctx.canvas.width = this.width;
+    this.ctx.canvas.height = this.height;
+	    
+  }
+  
+ 
+inicializa()
+{
+	
+	var self = this;
+	 
+addEventListener('resize', function resize(event){
+		
+    
+	self.width = window.innerWidth;
+    self.height = window.innerHeight;
+	self.ctx.canvas.width = self.width;
+	self.ctx.canvas.height = self.height;
+	console.table(self.ctx.width);
+	console.table(self.ctx.height);
+    self.desenha() 
+	
+	 /*this.width = window.innerWidth;
+    this.height = window.innerHeight;
+	this.ctx.width = this.width;
+	this.ctx.height = this.height;
+	console.table(this.ctx); */
+	
+}); 
+
+}
+start()
+{
+	
+	this.stars = [];
+	
+	for(var i=0; i<this.qtdestars; i++) {
+		
+			var x = Math.random()*this.width;
+			
+			var y =  Math.random()*this.height;
+			var size =  Math.random()*2+1;
+			var vel = (Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity;
+			
+			this.star = new Star(x,y, size,vel );
+			
+		this.stars[i] = this.star;
+	}
+
+	var timeUpdate = 1000 / this.fps;
+	var self = this;
+	this.intervalId = setInterval(function() {
+        self.update();
+        self.desenha();	
+    }, timeUpdate);
+}
+
+	desenha()
+	{
+	
+		this.ctx.fillStyle = '#4040ff';
+		this.ctx.fillRect(0, 0, this.width, this.height);
+		
+		
+		this.ctx.fillStyle = '#ffffff';
+		for(var i=0; i<this.stars.length;i++) {
+			this.star = this.stars[i];
+			this.ctx.fillRect(this.star.x, this.star.y, this.star.size, this.star.size);
+			
+		}
+		
+	}
+
+
+  update(){
+	var dt = 10 / this.fps;
+
+	for(var i=0; i<this.stars.length; i++) {
+		var star = this.stars[i];
+		star.y += dt * star.velocity;
+		
+		if(star.y > this.height) {
+			var x = Math.random()*this.width;
+			var size =  Math.random()*5+1;
+			var vel = (Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity;
+			let s = new Star(x, 0,size, vel);
+					
+		 	this.stars[i] = s;
+		}
+		
+	}
+	
+}
+
+stop()
+{
+	clearInterval(this.intervalId);
+}
+	
+}
