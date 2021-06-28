82164b858c3132ae1e5cce22d19ceaedf1d8d290
Caio Thomé Cruz <caio.cruz41@etec.sp.gov.br>
2021-06-27 22:49:12 -0300

    Inclusão

--- js/starfield.js
+++ js/starfield.js
@@ -1,8 +1,6 @@
-
-
 class Starfield
 {
-	
+	// define as variaveis
 	
 	fps ;
     canvas ;
@@ -20,23 +18,22 @@ class Starfield
 	 
   
   
-	
+	//define o construtor e o contexto
 	  constructor(ctx) {
-		  this.fps = 20;
+		  this.fps = 30;
 	
 	this.width = 0;
 	this.height = 0;
-	this.minVelocity = 10;
-	this.maxVelocity = 35;
-	this.qtdestars = 200;
+	this.minVelocity = 15;
+	this.maxVelocity = 30;
+	this.qtdestars = 100;
 	this.intervalId = 0;
 
     this.ctx = ctx;
-	
-	this.width = window.innerWidth;
-	console.table(window.innerWidth);
-	this.height = window.innerHeight;
-	console.table(window.innerHeight);
+	//console.table(this.ctx);
+	//define o tamanho da janela
+	this.width = 451;
+	this.height = 600;
 	this.ctx.canvas.width = this.width;
     this.ctx.canvas.height = this.height;
 	    
@@ -48,44 +45,29 @@ inicializa()
 	
 	var self = this;
 	 
-addEventListener('resize', function resize(event){
-		
-    
-	self.width = window.innerWidth;
-    self.height = window.innerHeight;
-	self.ctx.canvas.width = self.width;
-	self.ctx.canvas.height = self.height;
-	console.table(self.ctx.width);
-	console.table(self.ctx.height);
-    self.desenha() 
-	
-	 /*this.width = window.innerWidth;
-    this.height = window.innerHeight;
-	this.ctx.width = this.width;
-	this.ctx.height = this.height;
-	console.table(this.ctx); */
-	
-}); 
+
 
 }
 start()
 {
 	
 	this.stars = [];
-	
+	//console.table(stars);
 	for(var i=0; i<this.qtdestars; i++) {
-		
+		//console.table("start");
 			var x = Math.random()*this.width;
 			
 			var y =  Math.random()*this.height;
-			var size =  Math.random()*2+1;
+			var size =  Math.random()*3+1;
 			var vel = (Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity;
 			
 			this.star = new Star(x,y, size,vel );
-			
+			//console.table(s.x);
+			//console.table(this.star);
 		this.stars[i] = this.star;
 	}
-
+	
+//console.table(stars);
 	var timeUpdate = 1000 / this.fps;
 	var self = this;
 	this.intervalId = setInterval(function() {
@@ -96,37 +78,40 @@ start()
 
 	desenha()
 	{
-	
-		this.ctx.fillStyle = '#4040ff';
+		//console.table("teste");
+		//desenha o plano de fundo
+		this.ctx.fillStyle = '#000';
 		this.ctx.fillRect(0, 0, this.width, this.height);
 		
-		
-		this.ctx.fillStyle = '#ffffff';
+		//desenha as estrelas no plano de fundo
+		this.ctx.fillStyle = '#fff';
 		for(var i=0; i<this.stars.length;i++) {
 			this.star = this.stars[i];
 			this.ctx.fillRect(this.star.x, this.star.y, this.star.size, this.star.size);
+			//console.table(star);
+		}
+		
 	}
 
 
   update(){
-  
-	var dt = 10 / this.fps;
+	var dt = 1 / this.fps;
 
 	for(var i=0; i<this.stars.length; i++) {
 		var star = this.stars[i];
 		star.y += dt * star.velocity;
-		
+		//	If the star has moved from the bottom of the screen, spawn it at the top.
 		if(star.y > this.height) {
 			var x = Math.random()*this.width;
-			var size =  Math.random()*5+1;
+			var size =  Math.random()*3+1;
 			var vel = (Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity;
 			let s = new Star(x, 0,size, vel);
-					
+			//console.table(s);			
 		 	this.stars[i] = s;
 		}
 		
 	}
-	
+	//console.table(this.stars);
 }
 
 stop()
